de5063c892050ae3afdd7ad57d5debbc7061823d
martins <gabrielamspinola@gmail.com>
2022-03-20 03:12:08 -0300

    Primeiro Requisito incompleto, Segundo Requisito aprovado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,19 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const selectSpecie = species.find((specie) => specie.name === animal);
+
+  // console.log(selectSpecie);
+
+  const { residents } = selectSpecie;
+
+  // console.log(residents);
+
+  return residents.every((resident) => resident.age > age);
 }
 
 module.exports = getAnimalsOlderThan;
+
+// console.log(getAnimalsOlderThan('lions', 10));
