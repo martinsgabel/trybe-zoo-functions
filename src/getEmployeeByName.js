886827d0e69179f402422aa1c05ecdf933034e4e
martins <gabrielamspinola@gmail.com>
2022-03-20 04:06:54 -0300

    Requisito 1 e 3 incompleto, Requisito 2 aprovado

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,16 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
+function getEmployeeByName(...employeeName) {
   // seu código aqui
+  const employeeS = [];
+
+  // eslint-disable-next-line max-len
+  employees.find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
+
+  return employeeS;
 }
 
 module.exports = getEmployeeByName;
+
+console.log(getEmployeeByName('Burl'));
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -7,11 +7,6 @@ function getSpeciesByIds(...ids) {
   arrSpecies.push(ids.forEach((elemId) => species.find((specie) => specie.id === elemId)));
 
   return arrSpecies;
-
-  // const arrIds = [];
-  // ids.forEach((elemId) => arrIds.push(elemId));
-
-  // return arrIds;
 }
 
 module.exports = getSpeciesByIds;
@@ -22,3 +17,10 @@ module.exports = getSpeciesByIds;
 // console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 // eslint-disable-next-line max-len
 console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'));
+
+// RASCUNHO
+/* const arrIds = [];
+  ids.forEach((elemId) => arrIds.push(elemId));
+
+  return arrIds;
+*/
