4560cfb491db3bdd2b16d94b622d1649894e027d
martins <gabrielamspinola@gmail.com>
2022-03-20 02:00:40 -0300

    Primeiro Requisito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,16 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids === '') {
+    const emptyArr = [];
+    return emptyArr;
+  }
+
+  return ids.forEach((elemId) => species.find((specie) => specie.id === elemId));
 }
 
 module.exports = getSpeciesByIds;
+
+// eslint-disable-next-line max-len
+console.log(getSpeciesByIds());
