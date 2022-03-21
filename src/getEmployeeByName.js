cb3f285c5bdb1b4fb81a3c1fe77f46f1577971a0
martins <gabrielamspinola@gmail.com>
2022-03-21 19:35:39 -0300

    Requisito 1 e 2 aprovado e 3 refeito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,15 +1,22 @@
+/* eslint-disable max-len */
 const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(...employeeName) {
-  // seu código aqui
-  const employeeS = [];
-
-  employeeName.forEach((empName) => employeeS.push(employees.filter((employee) => employee.firstName === empName || employee.lastName === empName)));
-
-  return employeeS;
+function getEmployeeByName(employeeName) {
+  return employees.filter((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
 
-console.log(getEmployeeByName('Burl', 'Nelson'));
+console.log(getEmployeeByName('Burl'));
+console.log(getEmployeeByName('Nelson'));
+
+// employeeName.forEach((empName) => employees.filter((employee) => employee.firstName === empName || employee.lastName === empName));
+
+// let res;
+
+//   for (let index = 0; index < employeeName.length; index += 1) {
+//     res = employees.filter((employee) => employee.firstName === employeeName[index] || employee.lastName === employeeName[index]);
+//   }
+
+//   return res;
