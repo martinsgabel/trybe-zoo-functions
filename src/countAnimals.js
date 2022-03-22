2e77c74032f30ad48083593fb81605abd5534222
martins <gabrielamspinola@gmail.com>
2022-03-21 21:53:31 -0300

    Requisito 1, 2 e 3 aprovado, 4 iniciado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,9 +2,16 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  const { species } = data;
+  const { residents } = species;
 
+  if (!animal) {
+    return species.map((specie) => `${specie.name}: ${specie.residents.length}`);
+  }
+
+  
 }
 
 module.exports = countAnimals;
 
-console.log(countAnimals(animal));
+console.log(countAnimals());
