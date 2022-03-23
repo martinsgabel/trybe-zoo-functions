033b656f8850558471c1f4e40d87e6a462cd2822
martins <gabrielamspinola@gmail.com>
2022-03-23 18:40:30 -0300

    Req 9 Completo

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const empAnimals = employees.find((emp) => emp.id === id).responsibleFor;
+  const animals = species.find((specie) => empAnimals.includes(specie.id));
+  const res = animals.residents.reduce((acc, resident) => {
+    if (resident.age > acc.age) {
+      return resident;
+    }
+    return acc;
+  });
+  return Object.values(res);
 }
 
 module.exports = getOldestFromFirstSpecies;
+
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
