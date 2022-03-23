e994811a796477f72ba7a22006d654ccc2091536
martins <gabrielamspinola@gmail.com>
2022-03-23 13:44:17 -0300

    Metade do Req 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,19 +1,23 @@
-/* eslint-disable max-lines-per-function */
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(...animal) {
+function countAnimals(animal) {
   if (!animal) {
     const listAnimals = species.reduce((acc, currentValue) => {
       acc[currentValue.name] = currentValue.residents.length;
       return acc;
     }, {});
     return listAnimals;
-  } if (animal.length < 2) {
-    console.log(animal);
+  } if (!animal.sex) {
+    const objAnimal = species.find((specie) => specie.name === animal.specie);
+    const listAnimal = objAnimal.residents.length;
+    return listAnimal;
   }
+  // if (animal.specie && animal.sex) {
+  //   const objAnimal = species.find((specie) => specie.name === animal.specie);
+  // }
 }
 
 module.exports = countAnimals;
 
-console.log(countAnimals({ specie: 'penguins' }));
+// console.log(countAnimals({ specie: 'penguins' }))
--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,17 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalMap(options) {
   // seu código aqui
+  if (!options) {
+    const list = species.reduce((acc, currentValue) => {
+      acc[currentValue.location] = currentValue.name;
+      return acc;
+    }, {});
+    return list;
+  }
 }
 
 module.exports = getAnimalMap;
+
+console.log(getAnimalMap());
