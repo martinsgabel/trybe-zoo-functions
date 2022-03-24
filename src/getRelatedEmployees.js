4f9720981a3a6f00bb8f8433e613617ee0d3ac83
martins <gabrielamspinola@gmail.com>
2022-03-23 22:08:08 -0300

    Req 4 Completo

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,19 +1,21 @@
-const { /* species, */ employees } = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
   const isMang = employees.some((employee) => employee.managers.includes(id));
-
   return isMang;
 }
 
 function getRelatedEmployees(managerId) {
-  // se for gerente, array de pessoas nome completo dela
-  // se não lançar um Erro "O id inserido não é de uma pessoa colaboradora gerente!"
-  if (isManager) {
-    const listOfEmps = employees.filter((employee) => employee.managers.includes(managerId));
-
-    return listOfEmps;
+  try {
+    if (isManager(managerId)) {
+      const listOfEmps = employees.filter((employee) => employee.managers
+        .some((empManag) => empManag === managerId));
+      const res = listOfEmps.map((emp) => `${emp.firstName} ${emp.lastName}`);
+      return res;
+    } throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } catch (error) {
+    throw error.message;
   }
 }
 
